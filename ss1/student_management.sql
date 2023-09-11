150c7fcba885c1c7d78db9daba2f0c52ec1ac795
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-09-11 10:39:21 +0700

    Update student_management.sql
    1st commit

--- ss1/student_management.sql
+++ ss1/student_management.sql
@@ -7,10 +7,16 @@ age int,
 country VARCHAR(50)
 );
 
+INSERT INTO Students(student_name, age, country)
+VALUES ('Dao Duc Duy', 25, 'Vietnam'), ('Nguyen Van A', 2, 'Vietnam'), ('Elon Musk',60,'Vietnam');
+
 CREATE TABLE Class(
 id int primary key auto_increment,
 class_name varchar(50)
 );
+
+INSERT INTO Class(class_name)
+VALUES ('C0623G1'), ('C0723G1'), ('C0623G1');
 
 CREATE TABLE Teachers(
 	id int primary key auto_increment,
@@ -18,3 +24,6 @@ CREATE TABLE Teachers(
     age int,
     country varchar(50)
 );
+
+INSERT INTO Teachers(teacher_name, age, country)
+VALUES ('Teacher A', 21, 'Vietnam'), ('Teacher B', 27, 'Vietnam'), ('Teacher C',40,'India');
