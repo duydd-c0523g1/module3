76d543630f4f61589944a094cbc5ce219e282b86
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-09-14 16:06:02 +0700

    fix comment

--- ss3/bai_3-1.sql
+++ ss3/bai_3-1.sql
@@ -1,68 +1,44 @@
 CREATE DATABASE QuanLySinhVien;
 USE QuanLySinhVien;
-CREATE TABLE Class(
-
-         ClassID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-          ClassName VARCHAR(60) NOT NULL,
-
-          StartDate DATETIME NOT NULL,
-
-          Status BIT
-
+CREATE TABLE Class (
+    ClassID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ClassName VARCHAR(60) NOT NULL,
+    StartDate DATETIME NOT NULL,
+    Status BIT
 );
 
-CREATE TABLE Student(
-
-          StudentId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-          StudentName VARCHAR(30) NOT NULL,
-
-          Address VARCHAR(50),
-
-          Phone VARCHAR(20),
-
-          Status BIT,
-
-          ClassId INT NOT NULL,
-
-          FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
-
+CREATE TABLE Student (
+    StudentId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StudentName VARCHAR(30) NOT NULL,
+    Address VARCHAR(50),
+    Phone VARCHAR(20),
+    Status BIT,
+    ClassId INT NOT NULL,
+    FOREIGN KEY (ClassId)
+        REFERENCES Class (ClassID)
 );
 
-CREATE TABLE Subject(
-
-         SubId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-         SubName VARCHAR(30) NOT NULL,
-
-         Credit TINYINT NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
-
-         Status BIT DEFAULT 1
-
+CREATE TABLE Subject (
+    SubId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubName VARCHAR(30) NOT NULL,
+    Credit TINYINT NOT NULL DEFAULT 1 CHECK (Credit >= 1),
+    Status BIT DEFAULT 1
 );
 
-CREATE TABLE Mark(
-
-        MarkId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-        SubId INT NOT NULL,
-
-        StudentId INT NOT NULL,
-
-        Mark FLOAT DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
-
-        ExamTimes TINYINT DEFAULT 1,
-
-        UNIQUE (SubId, StudentId),
-
-        FOREIGN KEY (SubId) REFERENCES Subject (SubId),
-
-        FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
-
- );
+CREATE TABLE Mark (
+    MarkId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark FLOAT DEFAULT 0 CHECK (Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId , StudentId),
+    FOREIGN KEY (SubId)
+        REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId)
+        REFERENCES Student (StudentId)
+);
  
- SELECT 
+SELECT 
     StudentName
 FROM
     Student
@@ -74,7 +50,7 @@ SELECT
 FROM
     Class
 WHERE
-    StartDate LIKE '%12%';
+    MONTH(StartDate) = 12;
     
 SELECT 
     Credit
@@ -92,9 +68,9 @@ WHERE
 SELECT 
     Student.StudentName, sj.SubName, m.Mark
 FROM
-    Student
+    Student S
         JOIN
     Mark AS m ON m.StudentId = Student.StudentId
         JOIN
     Subject AS sj ON m.SubId = sj.SubId
-ORDER BY mark DESC
+ORDER BY mark DESC, SUBSTRING_INDEX(s.student_name, " ", -1) ASC;
