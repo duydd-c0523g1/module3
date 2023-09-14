0acdf2c764c0af8289b7ee7d862af4dcb4043454
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-09-14 15:31:38 +0700

    commit

--- case_study_1/bai_2-5.sql
+++ case_study_1/bai_2-5.sql
@@ -11,7 +11,7 @@ WHERE FLOOR(DATEDIFF(CURDATE(), dob) / 365) BETWEEN 18 AND 50 AND (address LIKE 
 SELECT c.customer_name, COUNT(c.customer_id) AS "time_booked"
 FROM customers c
 JOIN customer_type ct ON c.customer_type_id = ct.type_id
-LEFT JOIN contracts con ON c.customer_id = con.customer_id
+JOIN contracts con ON c.customer_id = con.customer_id
 WHERE ct.type_name = "diamond"
 GROUP BY c.customer_name
 ORDER BY COUNT(c.customer_id);
@@ -24,11 +24,12 @@ SELECT
     s.service_name,
     con.start_date,
     con.end_date,
-    (s.rental_fee + IFNULL(dc.quantity, 0) * IFNULL(asv.asv_price, 0)) AS total_price
-FROM customers AS c
+    (IFNULL(s.rental_fee, 0) + SUM(IFNULL(dc.quantity, 0) * IFNULL(asv.asv_price, 0))) AS total_price
+FROM customers c
 JOIN customer_type ct ON c.customer_type_id = ct.type_id
 LEFT JOIN contracts con ON c.customer_id = con.customer_id
 LEFT JOIN services s ON con.service_id = s.service_id
 LEFT JOIN detailed_contracts dc ON con.contract_id = dc.contract_id
 LEFT JOIN additional_services asv ON dc.asv_id = asv.asv_id
+GROUP BY c.customer_id, con.contract_id
 ORDER BY c.customer_id;
